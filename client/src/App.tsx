cce28959277d2985cb74f8474a8eb464ac97cd37
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-18 16:29:04 -0300

    Adiciona componente Home

--- client/src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div>Este é um teste</div>;
-};
-
-export default App;
---
+++ client/src/components/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>Teste</div>;
+};
+
+export default Home;
