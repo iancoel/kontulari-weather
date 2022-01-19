eaa9de68c364e8c9d3731a63e44c99125d2e144b
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-19 18:20:12 -0300

    Adiciona busca com autocomplete ao Header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -4,11 +4,24 @@ import {
   Title,
   HeaderButton,
 } from './styles';
+import { Autocomplete } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import citiesList from '../../utils/citiesList';
 
 const Header = () => {
   return (
     <HeaderContainer>
       <Title>Kontulari - Weather</Title>
+      <Autocomplete
+        id="cities"
+        freeSolo
+        size="small"
+        options={citiesList}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField {...params} placeholder="Insira o nome da cidade" />
+        )}
+      />
       <ButtonsContainer>
         <li>
           <HeaderButton variant="contained">Botão 1</HeaderButton>
