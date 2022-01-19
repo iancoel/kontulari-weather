7fa971f159655b7717d7926a6c954fa1985c08f9
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-19 12:24:21 -0300

    Adiciona infos da cidade ao CityCard com estilos básicos

--- client/src/components/CityCard/index.tsx
+++ client/src/components/CityCard/index.tsx
@@ -1,16 +1,34 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import api from '../../utils/api';
-import { CardContainer } from './styles';
+import { CardContainer, Title, Info, InnerCardContainer } from './styles';
 
 const CityCard = () => {
+  const [cityData, setCityData] = useState<any>();
+
   useEffect(() => {
     api
       .get('?location=Salvador')
-      .then((response) => console.log(response.data))
+      .then((response) => setCityData(response.data))
       .catch((error) => console.warn(error));
   }, []);
 
-  return <CardContainer></CardContainer>;
+  return (
+    <CardContainer>
+      <InnerCardContainer>
+        <Title>
+          {cityData?.title} / {cityData?.parent.title}
+        </Title>
+        <Info>{cityData?.time}</Info>
+        <Info>Nascer do sol às {cityData?.sun_rise}</Info>
+        <Info>Pôr do sol às {cityData?.sun_set}</Info>
+        <Info>
+          Fuso {cityData?.timezone} - {cityData?.timezone_name}
+        </Info>
+        <Info></Info>
+      </InnerCardContainer>
+      <InnerCardContainer></InnerCardContainer>
+    </CardContainer>
+  );
 };
 
 export default CityCard;
--- client/src/components/CityCard/styles.ts
+++ client/src/components/CityCard/styles.ts
@@ -3,7 +3,25 @@ import styled from 'styled-components';
 export const CardContainer = styled.div`
   min-height: 400px;
   width: 100%;
+  display: flex;
   margin-top: 50px;
   background: #633faa;
   border-radius: 10px;
+  color: #fbfbff;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-family: sans-serif;
+`;
+
+export const Info = styled.p`
+  font-size: 16px;
+  font-family: sans-serif;
+  margin-top: 20px;
+`;
+
+export const InnerCardContainer = styled.div`
+  width: 50%;
+  padding: 30px;
 `;
