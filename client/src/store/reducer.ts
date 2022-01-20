806b2759b8b070ec4d63bbce8f3c39da51d32422
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-19 21:09:02 -0300

    Corrige o reducer para capitalizar a string recebida

--- client/src/store/reducer.ts
+++ client/src/store/reducer.ts
@@ -3,7 +3,10 @@ import { IUpdateCityAction } from '../interfaces';
 const city = (state = '', action: IUpdateCityAction): string => {
   switch (action.type) {
     case 'ATUALIZAR_CIDADE':
-      return action.payload;
+      const capitalizedString =
+        action.payload.charAt(0).toUpperCase() + action.payload.slice(1);
+
+      return capitalizedString;
 
     default:
       return state;
