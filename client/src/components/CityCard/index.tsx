2a91dbae9295a658325db6e92abc11c52e3c78d2
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-21 09:02:38 -0300

    Remove condição loading mal implementada

--- client/src/components/CityCard/index.tsx
+++ client/src/components/CityCard/index.tsx
@@ -75,75 +75,42 @@ const CityCard = () => {
   }
 
   return (
-    <>
-      {loading && (
-        <CardContainer>
-          <InnerCardContainer>
-            <Skeleton variant="text" height={24} />
-            <Skeleton
-              variant="rectangular"
-              height={140}
-              style={{ marginTop: 20 }}
-            />
-          </InnerCardContainer>
-          <InnerCardContainer>
-            <Skeleton variant="text" height={24} />
-            <Skeleton
-              variant="rectangular"
-              height={140}
-              style={{ marginTop: 20 }}
-            />
-            <Skeleton
-              variant="rectangular"
-              height={140}
-              style={{ marginTop: 20 }}
-            />
-            <Skeleton
-              variant="rectangular"
-              height={140}
-              style={{ marginTop: 20 }}
-            />
-          </InnerCardContainer>
-        </CardContainer>
-      )}
-
-      <CardContainer>
-        <InnerCardContainer>
-          <Title>
-            {cityData?.title} / {cityData?.parent.title}
-          </Title>
-          <Info>Current time: {cityData?.time.slice(11, 19)}</Info>
-          <Info>Sunrise at {cityData?.sun_rise.slice(11, 19)}</Info>
-          <Info>Sunset at {cityData?.sun_set.slice(11, 19)}</Info>
-          <Info>
-            Timezone {cityData?.timezone.replace('_', ' ')} -{' '}
-            {cityData?.timezone_name}
-          </Info>
-          <Info>Sources:</Info>
-          {cityData?.sources.map((source, index) => (
-            <ButtonAnchor key={index} href={source.url} target="_blank">
-              <SourceButton>{source.title}</SourceButton>
-            </ButtonAnchor>
-          ))}
-        </InnerCardContainer>
-        <InnerCardContainer scrollable>
-          <Title>Forecasts</Title>
-          {cityData?.consolidated_weather.map((forecast: any, index: any) => (
-            <ForecastContainer key={index} divisors>
-              <Info first>{forecast.weather_state_name}</Info>
-              <Info>Applicable date: {forecast.applicable_date}</Info>
-              <Info>Wind Direction: {forecast.wind_direction_compass}</Info>
-              <Info>Wind Speed: {+forecast.wind_speed.toFixed(2)}mph</Info>
-              <Info>Min. Temp: {+forecast.min_temp.toFixed(0)}°C</Info>
-              <Info>Max. Temp: {+forecast.max_temp.toFixed(0)}°C</Info>
-              <Info>Current Temp: {+forecast.the_temp.toFixed(0)}°C</Info>
-              <Info>Air Pressure: {forecast.air_pressure}mb</Info>
-              <Info>Humidity: {forecast.humidity}%</Info>
-            </ForecastContainer>
-          ))}
-        </InnerCardContainer>
-      </CardContainer>
-    </>
+    <CardContainer>
+      <InnerCardContainer>
+        <Title>
+          {cityData?.title} / {cityData?.parent.title}
+        </Title>
+        <Info>Current time: {cityData?.time.slice(11, 19)}</Info>
+        <Info>Sunrise at {cityData?.sun_rise.slice(11, 19)}</Info>
+        <Info>Sunset at {cityData?.sun_set.slice(11, 19)}</Info>
+        <Info>
+          Timezone {cityData?.timezone.replace('_', ' ')} -{' '}
+          {cityData?.timezone_name}
+        </Info>
+        <Info>Sources:</Info>
+        {cityData?.sources.map((source, index) => (
+          <ButtonAnchor key={index} href={source.url} target="_blank">
+            <SourceButton>{source.title}</SourceButton>
+          </ButtonAnchor>
+        ))}
+      </InnerCardContainer>
+      <InnerCardContainer scrollable>
+        <Title>Forecasts</Title>
+        {cityData?.consolidated_weather.map((forecast: any, index: any) => (
+          <ForecastContainer key={index} divisors>
+            <Info first>{forecast.weather_state_name}</Info>
+            <Info>Applicable date: {forecast.applicable_date}</Info>
+            <Info>Wind Direction: {forecast.wind_direction_compass}</Info>
+            <Info>Wind Speed: {+forecast.wind_speed.toFixed(2)}mph</Info>
+            <Info>Min. Temp: {+forecast.min_temp.toFixed(0)}°C</Info>
+            <Info>Max. Temp: {+forecast.max_temp.toFixed(0)}°C</Info>
+            <Info>Current Temp: {+forecast.the_temp.toFixed(0)}°C</Info>
+            <Info>Air Pressure: {forecast.air_pressure}mb</Info>
+            <Info>Humidity: {forecast.humidity}%</Info>
+          </ForecastContainer>
+        ))}
+      </InnerCardContainer>
+    </CardContainer>
   );
 };
 
